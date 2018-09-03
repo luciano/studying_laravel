f17fb6c7922f60a607fc1c93d2f59a07f60fffd6
Luciano Silva <luciano.tj2011@gmail.com>
2018-09-03 18:23:30 -0300

    control the redirection of the login page if is user, go to the user login if is admin, go to the admin loginif logged in, go to the proper home page of each guard when tries to access the login page of the guard already logged.

--- multiauth/app/Exceptions/Handler.php
+++ multiauth/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -49,5 +50,27 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    // costumize the unauthenticated function
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
+        // send for the right login page according to the guard instead of sending for the user login
+        $guard = array_get($exception->guards(), 0);
+
+        switch ($guard) 
+        {
+            case 'admin':
+                $login = 'admin.login';
+                break;
+            default:
+                $login = 'login';
+        }
+
+        return redirect()->guest(route($login));
     }
 }
