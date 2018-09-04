3479998c86d60f76df4638fec59fc2d480718c50
Luciano Silva <luciano.tj2011@gmail.com>
2018-09-04 10:53:23 -0300

    using component and checking which kind of user is logged

--- multiauth/resources/views/home.blade.php
+++ multiauth/resources/views/home.blade.php
@@ -13,8 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in <strong>USER</strong>!
+                    @component('components.who')    
+                    @endcomponent
                 </div>
             </div>
         </div>
